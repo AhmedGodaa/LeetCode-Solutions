b43744f13df01ce812ff67f628e9be2a16161463
Ahmed Godaa <ana.a7med1140@gmail.com>
2022-11-01 21:08:34 +0200

    Time: 35 ms (33.82%), Space: 6.5 MB (8.38%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+          string number = to_string(x);
+    vector<char> vect(number.begin(), number.end());
+    auto rit = vect.rbegin();
+    int i = 0;
+    bool temp;
+
+    for (; rit != vect.rend(); rit++) {
+        if (vect[i] == *rit) {
+            temp = true;
+        } else {
+            temp = false;
+            break;
+
+        }
+        i++;
+    }
+    return temp;
+    }
+};
